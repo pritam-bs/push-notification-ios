329aceebaabaca06fd885b82a3b56384e0565f8e
Pritam Biswas <pritam.biswas@monstar-lab.com.bd>
2020-07-28 15:40:39 +0600

    [Task]: Remane test folder
    [Description]:
    Rename test folder
    
    Signed-off-by: Pritam Biswas <pritam.biswas@monstar-lab.com.bd>

--- PushNotificationUITests/PushNotificationUITests.swift
+++ PushNotificationUITests/PushNotificationUITests.swift
@@ -1,6 +1,6 @@
 //
-//  push_notification_iosUITests.swift
-//  push-notification-iosUITests
+//  PushNotificationUITests.swift
+//  PushNotificationUITests
 //
 //  Created by Pritam on 28/7/20.
 //  Copyright © 2020 MLBD. All rights reserved.
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class push_notification_iosUITests: XCTestCase {
+class PushNotificationUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
